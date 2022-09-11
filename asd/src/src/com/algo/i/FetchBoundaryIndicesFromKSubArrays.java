9b9b041a77b4656cae556c03697fa90081d5a860
Saroj Sahu <saroj.sahu@ibm.com>
2022-09-11 19:01:14 +0530

    Updating /FetchBoundaryIndicesFromKSubArrays.java LongestPalindromicSubstring.java

--- asd/src/src/com/algo/i/FetchBoundaryIndicesFromKSubArrays.java
+++ asd/src/src/com/algo/i/FetchBoundaryIndicesFromKSubArrays.java
@@ -3,31 +3,46 @@ package com.algo.i;
 public class FetchBoundaryIndicesFromKSubArrays {
     public static int[]
     fetchBoundaryIndices(int [] a, int k) {
-        int start = 0;
-        int end = start + k - 1;
         if (k > a.length) {
             return new int[] {-1, -1};
         }
-        for (int i = 1; i < a.length && start + k - 1 < a.length; ++i) {
+        int start = 0;
+        int end = start + k - 1;
+
+        for (int i = 1; end < a.length && i < end;) {
             int curr = a[i];
             if (curr >= a[start] || curr >= a[end]) {
                 start += 1;
                 end = start + k - 1;
+                i = start + 1;
             } else {
                 if (i == end - 1) {
                     return new int[] {start, end};
                 }
+                ++i;
             }
         }
         return new int[] {-1, -1};
     }
 
     public static void main(String[] args) {
-        int a[] = {1, 2, 4, 2, 3, 1, 3, 1, 5, 4, 2, 6};
-        //int[] array = {2, 3, 4, 5, 6, 7, 8};
-        int[] array = {2, 3, 4, 5, 6, 7, 6, 5, 9};
-        int k = 4;
-        int[] indices = fetchBoundaryIndices(a, k);
-        int[] indices2 = fetchBoundaryIndices(array, k);
+        int[][] arrayList = {
+
+                {1, 2, 4, 2, 3, 1, 3, 1, 5, 4, 2, 6},
+                {2, 3, 4, 5, 6, 7, 6, 5, 9},
+                {3, 4, 2, 1, 2, 10, 9, 7, 6, 8, 5, 9, 10},
+                {3, 4, 5, 1, 2, 10, 9, 7, 6, 8, 9, 9, 10},
+        };
+        int k = 5;
+        for (int[] array : arrayList) {
+            System.out.println("Array : ");
+            for (int num : array) {
+                System.out.printf("%d, ", num);
+            }
+            System.out.println();
+            int[] indices = fetchBoundaryIndices(array, k);
+            System.out.println("Indices : " + indices[0] + ", " + indices[1]);
+            System.out.println();
+        }
     }
 }
--- asd/src/src/com/algo/lc/backtrackordp/LongestPalindromicSubstring.java
+++ asd/src/src/com/algo/lc/backtrackordp/LongestPalindromicSubstring.java
@@ -61,7 +61,7 @@ public class LongestPalindromicSubstring {
         }
         return right - left - 1;
     }
-    
+
     public static void main(String[] args) {
         String s = "abcmnnmcb";
         String longestPalindromicSubstring = longestPalindrome(s);
