39773f6701dd9df1f2cc97242419eba4e1e832b1
Saroj Sahu <saroj.sahu@ibm.com>
2023-01-02 19:24:32 +0530

    Adding RemoveDuplicatesFromSortedArray

---
+++ asd/src/src/com/algo/lc/topintq/array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,93 @@
+package com.algo.lc.topintq.array;
+
+import com.algo.util.Utils;
+
+/**
+ * Remove Duplicates from Sorted Array
+ *
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+ *
+ * Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+ *
+ * Return k after placing the final result in the first k slots of nums.
+ *
+ * Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * Custom Judge:
+ *
+ * The judge will test your solution with the following code:
+ *
+ * int[] nums = [...]; // Input array
+ * int[] expectedNums = [...]; // The expected answer with correct length
+ *
+ * int k = removeDuplicates(nums); // Calls your implementation
+ *
+ * assert k == expectedNums.length;
+ * for (int i = 0; i < k; i++) {
+ *     assert nums[i] == expectedNums[i];
+ * }
+ * If all assertions pass, then your solution will be accepted.
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,2]
+ * Output: 2, nums = [1,2,_]
+ * Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ * Example 2:
+ *
+ * Input: nums = [0,0,1,1,1,2,2,3,3,4]
+ * Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+ * Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+public class RemoveDuplicatesFromSortedArray {
+    /**
+     *
+     * Complexity Analysis
+     * Let N be the size of the input array.
+     *
+     * Time Complexity: O(N), since we only have 2 pointers, and both the pointers will traverse the array at most once.
+     *
+     * Space Complexity: O(1), since we are not using any extra space.
+     */
+    public static int removeDuplicates(int[] nums) {
+        int i = 1, j = 0;
+        while (i < nums.length) {
+            if (nums[i] != nums[j]) {
+                ++j;
+                nums[j] = nums[i];
+            }
+            ++i;
+        }
+        return j + 1;
+    }
+
+    public static int removeDuplicatesF(int[] nums) {
+        int insertIndex = 1;
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i - 1] != nums[i]) {
+                nums[insertIndex++] = nums[i];
+            }
+        }
+        return insertIndex;
+    }
+
+    public static void main(String[] args) {
+        int[][] arrays = {
+                {0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+                {0, 1},
+                {1},
+        };
+        for (int[] arr : arrays) {
+            System.out.println("Original array : ");
+            Utils.print1DArray(arr);
+            int k = removeDuplicatesF(arr);
+            System.out.println("Length of newly sorted array : " + k);
+            System.out.println("Array with first " + k + " contents: ");
+            Utils.print1DArray(arr);
+            System.out.println("Done.");
+            System.out.println();
+        }
+    }
+}
---
+++ asd/src/src/com/algo/util/Utils.java
@@ -0,0 +1,10 @@
+package com.algo.util;
+
+public class Utils {
+    public static void print1DArray(int[] arr) {
+        for (int a : arr) {
+            System.out.printf("%d, ", a);
+        }
+        System.out.println();
+    }
+}
