3f3d4bd8e2672849eae7b0ea51cbf464fc260a23
Saroj Sahu <saroj.sahu@ibm.com>
2023-01-16 09:04:30 +0530

    Adding ValidSubsequence.java

--- asd/src/src/com/algo/ae/arrays/TwoNumberSum.java
+++ asd/src/src/com/algo/ae/arrays/TwoNumberSum.java
@@ -4,6 +4,20 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Write a function that takes in a non-empty array of distinct integers and an
+ * integer representing a target sum. If any 2 numbers in the input array sum up
+ * to the target sum, the function should return them in an array, in any order.
+ * If no two numbers sum up to the target sum, the function should return an empty
+ * array.
+ *
+ * Note that the target sum has to be obtained by summing 2 different integers in
+ * the array; you can't add a single integer to itself in order to obtain the
+ * target sum.
+ *
+ * You can assume that there will be at most 1 pair of numbers summing up to the
+ * target sum.
+ */
 public class TwoNumberSum {
 
     // Time O(N^2), space : O(1)
---
+++ asd/src/src/com/algo/ae/arrays/ValidSubsequence.java
@@ -0,0 +1,44 @@
+package com.algo.ae.arrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Validate Subsequence
+ *
+ * Given 2 non-empty arrays of integers, write a function that determines if the
+ * second array is a subsequence of the first one.
+ *
+ * A subsequence of an array is a set of numbers that aren't necessarily adjacent
+ * in the array but that are in the same order as they appear in the array. For
+ * instance, the numbers [1, 3, 4] form a subsequence of the array [1, 2, 3, 4],
+ * and so do the numbers [2, 4]. Note that a single number in an array and the
+ * array itself are both valid subsequences of the array.
+ */
+public class ValidSubsequence {
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        if (sequence.size() > array.size()) {
+            return false;
+        }
+        int arrIdx = 0;
+        int seqIdx = 0;
+        while (arrIdx < array.size() && seqIdx < sequence.size()) {
+            if (array.get(arrIdx).equals(sequence.get(seqIdx))) {
+                ++seqIdx;
+            }
+            ++arrIdx;
+        }
+        return seqIdx == sequence.size();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> array = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10);
+        List<Integer> sequence = Arrays.asList(1, 6, -1, 10);
+
+        if (isValidSubsequence(array, sequence)) {
+            System.out.println("Sequence array is a valid subsequence inside array");
+        } else {
+            System.out.println("Sequence array is not a valid subsequence inside array");
+        }
+    }
+}
