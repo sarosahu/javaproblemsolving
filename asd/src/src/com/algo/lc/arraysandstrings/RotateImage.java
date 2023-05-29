cb64748cdeb474122ee42c731184822bd101dc52
Saroj Kumar Sahu <sarojkumarsahu@Sarojs-MacBook-Pro.local>
2023-05-29 09:20:52 +0530

    Adding few comments to RotateImage

--- asd/src/src/com/algo/lc/arraysandstrings/RotateImage.java
+++ asd/src/src/com/algo/lc/arraysandstrings/RotateImage.java
@@ -12,6 +12,10 @@ package com.algo.lc.arraysandstrings;
  * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
  * Output: [[7,4,1],[8,5,2],[9,6,3]]
  *
+ * 1 2 3    7 4 1
+ * 4 5 6 => 8 5 2
+ * 7 8 9    9 6 3
+ *
  *
  * Example 2:
  *
