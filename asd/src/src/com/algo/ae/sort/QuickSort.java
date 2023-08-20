8b5f2ac4434254f0595df5f2808384a83bc534c8
Saroj Kumar Sahu <sarojkumarsahu@Sarojs-MBP.domain.name>
2023-08-20 05:58:23 +0530

    Updating QuickSort.java

--- asd/src/src/com/algo/ae/sort/QuickSort.java
+++ asd/src/src/com/algo/ae/sort/QuickSort.java
@@ -1,5 +1,7 @@
 package com.algo.ae.sort;
 
+import java.util.Arrays;
+
 /**
  * Quick Sort
  *
@@ -12,6 +14,7 @@ package com.algo.ae.sort;
  */
 
 public class QuickSort {
+    // {{{
     public static int[] quickSort(int[] array) {
         // Write your code here.
         quickSort(array, 0, array.length - 1);
@@ -41,10 +44,56 @@ public class QuickSort {
         quickSort(array, start, right - 1);
         quickSort(array, right + 1, end);
     }
+    // }}}
+
+    // {{{
+    /**
+     *
+     * @param array
+     * Note: This approach is elegant, I like it.
+     */
+    public static void quickSort2(int[] array) {
+        // Write your code here.
+        quickSort2(array, 0, array.length - 1);
+    }
+    private static void quickSort2(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int pivotIdx = partition(arr, start, end);
+        quickSort2(arr, start, pivotIdx - 1);
+        quickSort2(arr, pivotIdx + 1, end);
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        int pos = start;
+        for (int i = start; i < end; ++i) {
+            if (arr[i] < pivot) {
+                swap(arr, i, pos);
+                ++pos;
+            }
+        }
+        // Now swap the pos with end, so that pivot position is fixed
+        swap(arr, pos, end);
+
+        return pos;
+    }
+    // }}}
 
     public static void swap(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int [] arr1 = {100, 2, 90, 80, 1, -5, 20, 3, 60, 50, 99};
+        quickSort(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+        int [] arr2 = {100, 2, 90, 80, 1, -5, 20, 3, 60, 50, 99};
+        quickSort2(arr2);
+        System.out.println(Arrays.toString(arr2));
     }
 }
