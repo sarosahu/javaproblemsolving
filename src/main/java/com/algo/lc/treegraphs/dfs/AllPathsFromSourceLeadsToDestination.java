8dbacd0fda3486afc974da8fb1f5262c014899f4
Saroj Kumar Sahu <sarojkumarsahu@Sarojs-MBP.domain.name>
2023-09-11 06:20:08 +0530

    updating dfs/AllPathsFromSourceLeadsToDestination.java -- putting questions with example

--- src/main/java/com/algo/lc/treegraphs/dfs/AllPathsFromSourceLeadsToDestination.java
+++ src/main/java/com/algo/lc/treegraphs/dfs/AllPathsFromSourceLeadsToDestination.java
@@ -3,6 +3,49 @@ package com.algo.lc.treegraphs.dfs;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 1059. All Paths from Source Lead to Destination
+ *
+ * Given the edges of a directed graph where edges[i] = [ai, bi] indicates there is an edge
+ * between nodes ai and bi, and two nodes source and destination of this graph, determine
+ * whether or not all paths starting from source eventually, end at destination, that is:
+ *
+ * - At least one path exists from the source node to the destination node
+ * - If a path exists from the source node to a node with no outgoing edges, then that node
+ *   is equal to destination.
+ * - The number of possible paths from source to destination is a finite number.
+ *
+ * Return true if and only if all roads from source lead to destination.
+ *
+ * Example 1:
+ *  n = 3, Source : 0, Destination : 2
+ *                0 --> 2
+ *                |
+ *                |
+ *                v
+ *                1
+ *  Output: false
+ *
+ *  Example 2:
+ *  Input: n = 4, edges = [[0,1],[0,3],[1,2],[2,1]], source = 0, destination = 3
+ * Output: false
+ * Explanation: We have two possibilities: to end at node 3, or to loop over node 1 and node 2 indefinitely.
+ *
+ * Example 3:
+ * Input: n = 4, edges = [[0,1],[0,2],[1,3],[2,3]], source = 0, destination = 3
+ * Output: true
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 104
+ * 0 <= edges.length <= 104
+ * edges.length == 2
+ * 0 <= ai, bi <= n - 1
+ * 0 <= source <= n - 1
+ * 0 <= destination <= n - 1
+ *
+ * Note: The given graph may have self-loops and parallel edges.
+ */
 public class AllPathsFromSourceLeadsToDestination {
     enum State {Unvisited, Visiting, Visited};
 
